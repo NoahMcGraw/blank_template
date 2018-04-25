2a536bfdffa3313bd04e317f61499da379aaaf87
Noah McGraw <McMoldy96@gmail.com>
2018-04-24 19:49:22 -0500

    Fixes to indentations; Some renaming of assets done.

--- app/app.js
+++ app/app.js
@@ -3,11 +3,11 @@ app.config(function($mdThemingProvider) {
   var primaryColorMap =
     $mdThemingProvider.extendPalette('cyan', {
     'contrastDefaultColor': 'light'
-    }); 
+    });
   var accentColorMap =
-    $mdThemingProvider.extendPalette('light-green', {
+    $mdThemingProvider.extendPalette('grey', {
     'contrastDefaultColor': 'light'
-    }); 
+    });
   $mdThemingProvider.definePalette('primaryColor', primaryColorMap);
   $mdThemingProvider.definePalette('accentColor', accentColorMap);
   $mdThemingProvider.theme('default')
@@ -21,28 +21,16 @@ app.config(function($mdThemingProvider) {
       'hue-1': 'A200',
       'hue-2': '800'
     });
-    
   $mdThemingProvider.theme('dark')
-
     .primaryPalette('primaryColor', {
-
       'default': '500',
-
       'hue-1': 'A200',
-
       'hue-2': '700'
-
     })
-
     .accentPalette('accentColor', {
-
       'default': '700',
-
       'hue-1': 'A200',
-
       'hue-2': '800'
-
     })
-
     .dark();
 });
